4773a2094d5bb1907869f06e53c2d3b53955511b
Jonathan Williams <jtw365@live.missouristate.edu>
2018-04-14 21:53:32 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Fish
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+    @fishes = []
+  end
+
+  def self.buy_fish(fish)
+    @fishes << fish
+    fish.owner = self.
+    #binding.pry
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class Owner
-  # code goes here
+
+  @@all = []
+
+
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  def initialize(species)
+    @species = species
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+
 end
